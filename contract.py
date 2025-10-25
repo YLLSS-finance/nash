9589f8ba90a94bd5e23701654e2007ec3454cace
yllssnospinning <mrnospinning@gmail.com>
2025-10-25 16:01:27 +0800

    update order adding mechanism

--- contract.py
+++ contract.py
@@ -6,15 +6,18 @@ import time as t
 class accountContract:
     def __init__(self):
         self._master = None
-        
+        self.contractID = 'contract'
+        self._id = 0
         # exposing some functions and objects from the account class
-        self.mpid = self._master.mpid
+        #self.mpid = self._master.mpid
+        self.mpid = 'bob'
         
         # adds an order object to account's order dictionary and respective exchange orderbook
-        self.addOrder = self._master.addOrder
+        #self.addOrder = self._master.addOrder
+        self.globalAddOrder = print
         
         # Change base token amount for transactions
-        self.chg = self._master.chg
+        self.chg = print
         
         self.position = [0, 0]
         self.reducible = [0, 0]
@@ -29,12 +32,13 @@ class accountContract:
 
     def addOrder(self, price, side, qty):
         red, inc = self.getNewOrderQty(side, qty)
-        
         # TODO: CHECK COST
         
+        self.reducible[1 - side] -= red
         ordr = order(
             _master = self,
-            orderID = id, 
+            orderID = self._id, 
+            contractID = self.contractID,
             mpid = self.mpid, 
             timestamp = round(1000 * t.time()), 
             price = price, 
@@ -42,10 +46,26 @@ class accountContract:
             inc = inc,
             red = red
         )
+        self._id += 1
         redOrders, incOrders = self.red[side], self.inc[side]
         
-        while redOrders[-1] > ordr:
+        while True:
+            if len(redOrders) == 0:
+                break
+            result = redOrders[-1].convertOrdr(ordr)
+            if result is False:
+                break
             
-        
+        self.globalAddOrder(ordr)
+        self.chg(ordr.incCost * ordr.inc * -1)
+        ordr.updateOrder()
 
-        
+test = accountContract()
+val = [0, 25]
+test.position = val
+test.reducible = val
+test.addOrder(8, 0, 10)
+test.addOrder(7, 0, 10)
+print('red', test.reducible)
+print(test.red[0])
+test.addOrder(9, 0, 10)
--- order.py
+++ order.py
@@ -15,39 +15,48 @@ class order:
         self.incCost = price if self.side == 0 else 100 - self.price
         self.comparisonKey = (-self.price if self.side == 0 else self.price, self.timestamp, self.orderID)
         
-        self.updateOrder
     
     def __lt__(self, other):
-        return self.comparisonKey 
+        return self.comparisonKey < other.comparisonKey
     
     def updateOrder(self):
         red = self._master.red[self.side]
         inc = self._master.inc[self.side]
-        if ordr.red != 0:
+        if self.red != 0:
             red.add(self)
-        if ordr.red == 0:
+        if self.red == 0:
             red.discard(self)
-        if ordr.inc != 0:
+        if self.inc != 0:
             inc.add(self)
-        if ordr.inc == 0:
+        if self.inc == 0:
             inc.discard(self)
     
     def move(self, qty):
         '''Transfers reduce quantity to increase quantity for a positive value and vice versa.'''
         self.inc += qty
         self.red -= qty
+        self.updateOrder()
     
     
     def convertOrdr(self, incOrdr):
-        '''Shifts reduce quantity of incoming order to increase quantity. Will cause magin allocation.'''
+        '''Shifts reduce quantity of incoming order to increase quantity. Will cause margin allocation.'''
         if incOrdr.inc == 0:
             return False
-        if incOrdr
+        
+        # Check if the reducing order already takes priority
+        if incOrdr > self:
+            print('red has prio')
+            return False
                 
         chgQty = min(incOrdr.inc, self.red)
+        
+        if chgQty == 0:
+            return False
+        
         self.move(chgQty)
         incOrdr.move(-chgQty)
         
+        print('PAYMENT')
         self._master.chg(-self.incCost * chgQty)
         self.updateOrder()
 
